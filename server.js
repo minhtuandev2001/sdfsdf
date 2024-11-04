0d20d478123f5b4ea3c56654d2de088bb8c7c79b
minhtuan2001 <minhtuandev2001@gmail.com>
2024-11-05 03:14:16 +0700

    connect db

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./src/config/db.js"
 
 // config dotenv
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
 const PORT = process.env.PORT || 4000
 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
+  credentials: true
+}));
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("hello world!")
+  res.send("hello world!!!!!!!")
 })
 
 app.listen(PORT, () => {
